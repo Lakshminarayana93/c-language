1d5d2f0e423b8b92f2f0fb35ff13b60d1536798f
Lakshminarayana93 <lakshminarayanasaiadimulam@gmail.com>
2024-08-17 21:45:06 +0530

    Add files via upload

---
+++ c language 011.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+int roll_no;
+float height;
+char gender,name[20];
+printf("Enter roll no...:");
+scanf("%d",&roll_no);
+printf("Enter height...:");
+scanf("%f",&height);
+printf("Enter The Name...:");
+scanf("%f",&name);
+fflush(stdin);
+printf("Enter gender...:");
+scanf("%c",&gender);
+printf("Roll no Occupies %d bytes of space",sizeof(roll_no));
+printf("\nheight Occupies %d bytes of space",sizeof(height));
+printf("\nName Occupies %d bytes of space",sizeof(name));
+printf("\ngender Occupies %d bytes of space",sizeof(gender));
+}
