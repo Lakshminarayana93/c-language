d1e70b6ae30b7ef494409a1fee51519d40ac34bc
Lakshminarayana93 <lakshminarayanasaiadimulam@gmail.com>
2024-08-18 19:35:13 +0530

    Add files via upload

---
+++ c language 047.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+main()
+{
+char ch;
+printf("Enter a character: ");
+scanf("%c", &ch);
+if (isupper(ch))
+{
+printf("The character '%c' is a capital letter.\n", ch);
+}
+else
+if (islower(ch))
+{
+printf("The character '%c' is a small letter.\n", ch);
+}
+else
+if (isdigit(ch))
+{
+printf("The character '%c' is a digit.\n", ch);
+}
+else
+{
+printf("The character '%c' is a special character.\n", ch);
+}
+  }
---
+++ c language 051.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+main()
+{
+char gender;
+int age;
+printf("enter gender of person (M/F) :-");
+scanf("%s",&gender);
+printf("enter age of person :-");
+scanf("%d",&age);
+if (age<=5)
+{
+printf("you have full concession!!");
+}
+else
+if (gender=='M' || gender=='m' && age>60)
+{
+printf("SCR GIVES 50% CONCESSION !!");
+}
+else
+if(gender=='F' || gender=='f' && age>50)
+{
+printf("SCR GIVES 50% CONCESSION");
+}
+else
+{
+printf("SCR GIVES NO CONCESSION");
+}
+}
